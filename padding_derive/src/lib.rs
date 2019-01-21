956322c1cb22b2495b856545a9899f3db351e001
Kitsu <mail@kitsu.me>
2019-01-21 12:47:19 +0300

    Simplify code in Padder deriving

--- padding_derive/src/lib.rs
+++ padding_derive/src/lib.rs
@@ -14,17 +14,9 @@ pub fn padder_derive(input: TokenStream) -> TokenStream {
     match ast.data {
         Struct(s) => {
             let types = || s.fields.iter().map(|f| &f.ty);
+            let sizes = types().map(|ty| quote!(std::mem::size_of::<#ty>()));
             let tys = types();
-            let tys_ = types();
-            let next_aligns1 = types()
-                .map(|ty| quote!(<#ty>::alignof()))
-                .skip(1)
-                .chain(std::iter::once(quote!(#name::alignof())));
-            let next_aligns2 = types()
-                .map(|ty| quote!(<#ty>::alignof()))
-                .skip(1)
-                .chain(std::iter::once(quote!(#name::alignof())));
-            let next_aligns3 = types()
+            let next_aligns = types()
                 .map(|ty| quote!(<#ty>::alignof()))
                 .skip(1)
                 .chain(std::iter::once(quote!(#name::alignof())));
@@ -36,13 +28,14 @@ pub fn padder_derive(input: TokenStream) -> TokenStream {
                           let mut buf = [0xff; std::mem::size_of::<#name>()];
                           let mut offset = 0;
                           #({
-                            let size = std::mem::size_of::<#tys>();
+                            let size = #sizes;
                             let next_offset = offset + size;
-                            let padding_size = next_offset % #next_aligns1;
-                            let padding_size = (#next_aligns2 - padding_size) % #next_aligns3;
+                            let next_align = #next_aligns;
+                            let padding_size = next_offset % next_align;
+                            let padding_size = (next_align - padding_size) % next_align;
                             let next_offset = next_offset + padding_size;
                             let mut count = 0;
-                            for (i, b) in <#tys_>::padding_buf().iter().enumerate() {
+                            for (i, b) in <#tys>::padding_buf().iter().enumerate() {
                               buf[offset + i] = *b;
                               count += 1;
                             }
